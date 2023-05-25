fa2205d88a6f7f5149f95fb75def068aa56d1767
Bob Esponja <Bob Esponja>
2023-05-25 19:47:43 +0200

    .

--- modules/db.js
+++ modules/db.js
@@ -1,26 +1,29 @@
+const dburl = `https://dungee-1c560-default-rtdb.europe-west1.firebasedatabase.app`;
+
+
 export function fetchUsuarios(pin){
-    return fetch(`/api/damelosusers?pin=${pin}`).then(datos => datos.json())
+    return fetch(`${dburl}/api/damelosusers?pin=${pin}`).then(datos => datos.json())
 }
 
 export function fetchPreguntas(){
-    return fetch(`/api/damelasquestions`).then(preguntitas => preguntitas.json())
+    return fetch(`${dburl}/api/damelasquestions`).then(preguntitas => preguntitas.json())
 }
 
 export function updatePreguntaActual(i, pin){
-    fetch (`https://dungee-1c560-default-rtdb.europe-west1.firebasedatabase.app/pins/pin${pin}/pregunta.json`, {
+    fetch (`${dburl}/pins/pin${pin}/pregunta.json`, {
         method: 'PUT',
         body: `{"value": ${i}}`
     });
 }
 
 export function addUser(pin, nom){
-    fetch (`https://dungee-1c560-default-rtdb.europe-west1.firebasedatabase.app/pins/pin${pin}/users.json`, {
+    fetch (`${dburl}/pins/pin${pin}/users.json`, {
             method: 'POST',
             body: `{"name": "${nom}" }`
     });
 }
 
 export function getPreguntaActual(pin){
-    return fetch(`https://dungee-1c560-default-rtdb.europe-west1.firebasedatabase.app/pins/pin${pin}/pregunta.json`)
+    return fetch(`${dburl}/pins/pin${pin}/pregunta.json`)
         .then(data => data.json())
 }
