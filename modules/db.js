6bd7c5194625138a782468455a9d576ad8f5564a
Bob Esponja <Bob Esponja>
2023-05-29 19:52:53 +0200

    .

--- modules/db.js
+++ modules/db.js
@@ -2,15 +2,55 @@ const dburl = `https://dungee-252d3-default-rtdb.europe-west1.firebasedatabase.a
 
 
 export function fetchUsuarios(pin){
-    return fetch(`/api/damelosusers?pin=${pin}`).then(datos => datos.json())
+    return fetch(`${dburl}/pins/pin${pin}.json`)
+    .then(data => data.json())
+    // return fetch(`/api/damelosusers?pin=${pin}`).then(datos => datos.json())
 }
 
-export function fetchPreguntas(){
-    return fetch(`/api/damelasquestions`).then(preguntitas => preguntitas.json())
+export async function fetchPreguntas(){
+    return {
+        "preguntas": {
+            "pregunta0" : {
+                "pregunta": "2+2?",
+                "respuesta0": "4",
+                "respuesta1": "5",
+                "respuesta2": "2",
+                "respuesta3": "1",
+                "correcta": 0
+            },
+            "pregunta1" : {
+                "pregunta": "2+7?",
+                "respuesta0": "4",
+                "respuesta1": "5",
+                "respuesta2": "9",
+                "respuesta3": "1",
+                "correcta": 2
+            },
+            "pregunta2" : {
+                "pregunta": "2+1?",
+                "respuesta0": "4",
+                "respuesta1": "3",
+                "respuesta2": "2",
+                "respuesta3": "1",
+                "correcta": 1
+            },
+            "pregunta3" : {
+                "pregunta": "20+10?",
+                "respuesta0": "40",
+                "respuesta1": "10",
+                "respuesta2": "20",
+                "respuesta3": "30",
+                "correcta": 3
+            }
+        }
+    }
+    // return fetch(`/api/damelasquestions`).then(preguntitas => preguntitas.json())
 }
 
 export function getPreguntaActual(pin){
-    return fetch(`/api/damelapreguntaactual.js`).then(preguntitas => preguntitas.json())
+    return fetch(`${dburl}/pins/pin${pin}/pregunta.json`)
+    .then(data => data.json());
+    // return fetch(`/api/damelapreguntaactual.js`).then(preguntitas => preguntitas.json())
 }
 
 export function updatePreguntaActual(i, pin){
@@ -21,9 +61,19 @@ export function updatePreguntaActual(i, pin){
 }
 
 export function addUser(pin, nom){
-    fetch (`${dburl}/pins/pin${pin}/users.json`, {
+    return fetch (`${dburl}/pins/pin${pin}/users.json`, {
             method: 'POST',
             body: `{"name": "${nom}" }`
-    });
+    })
+    .then(data => data.json());;
+}
+
+export function actualizarRespuesta(pin, userid, respuesta, pregunta){
+    fetch(`${dburl}/pin/pin${pin}/users/${userid}/pregunta${pregunta}.json`, {
+        method: 'PUT',
+        body: `{"value": "${respuesta}"}`
+    })
+
+
 }
 
